5ba21ca37d995f6fa0dc75ab3d5cedefc0813c35
Cillian Brewitt <cillianbrewitt@gmail.com>
2022-06-15 23:20:26 +0100

    add timing to preprocess one episode
    Former-commit-id: 000045cbd335fd1820566c2a6ab47884d6c3e02b

--- scripts/preprocess_one_episode.py
+++ scripts/preprocess_one_episode.py
@@ -1,3 +1,5 @@
 from grit.core.data_processing import prepare_episode_dataset
-
-prepare_episode_dataset(('round', 1, True))
+from datetime import datetime
+start = datetime.now()
+prepare_episode_dataset(('bendplatz', 0, True))
+print(datetime.now() - start)
