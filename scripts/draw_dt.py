78ced40fdfb1d3e9fe6a5ad424cd76a0533c257b
Cillian Brewitt <cillianbrewitt@gmail.com>
2022-06-20 19:48:41 +0100

    changes to plotting
    Former-commit-id: 0c0a9139d44c6f01bbdb8ca5b3db8b5f710d0dfd

--- scripts/draw_dt.py
+++ scripts/draw_dt.py
@@ -1,10 +1,13 @@
 from grit.core.base import get_img_dir
-from grit.decisiontree.dt_goal_recogniser import OcclusionGrit
+from grit.decisiontree.dt_goal_recogniser import OcclusionGrit, GeneralisedGrit
 
 model = OcclusionGrit.load('heckstrasse')
-truncate = ['RT', 'RFTT', 'RFFF']
-goal_type = 'exit-roundabout'
+model.save_images()
+# truncate = ['RT', 'RFTT', 'RFFF']
+truncate = []
+goal_type = 'turn-right'
 
 goal_tree = model.decision_trees[goal_type]
+
 pydot_tree = goal_tree.pydot_tree(truncate_edges=truncate)
 pydot_tree.write_pdf(get_img_dir() + f'{model.get_model_name()}_{goal_type}.pdf')
