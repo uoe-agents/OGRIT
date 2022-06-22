663c78ba433cd9061e867c4bfaf038b8bc2831cc
Massimiliano <s2105338@ed.ac.uk>
2022-06-22 18:57:14 +0100

    Added script to get the samples for only 1 episode
    Former-commit-id: 644bcdb52e8f937c46f81d1bf46bed4d581d0f7c

--- scripts/preprocess_one_episode.py
+++ scripts/preprocess_one_episode.py
@@ -1,5 +1,22 @@
-from grit.core.data_processing import prepare_episode_dataset
+import argparse
 from datetime import datetime
-start = datetime.now()
-prepare_episode_dataset(('bendplatz', 0, True))
-print(datetime.now() - start)
+
+from grit.core.data_processing import prepare_episode_dataset
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process the dataset')
+    parser.add_argument('--scenario', type=str, help='Name of scenario to process', default="bendplatz")
+    parser.add_argument('--episode_idx', type=int, help='Name of scenario to process', default=0)
+    parser.add_argument('--extract_indicator_features', help='If you want to extract the indicator features',
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    start = datetime.now()
+    prepare_episode_dataset((args.scenario, args.episode_idx, args.extract_indicator_features))
+    print(datetime.now() - start)
+
+
+if __name__ == '__main__':
+    main()
