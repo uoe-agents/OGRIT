1a16f0f76661ba6aec82d53fcf35da687c1e1f97
Cillian Brewitt <cillianbrewitt@gmail.com>
2022-06-22 17:42:56 +0100

    remove name from setup
    Former-commit-id: c6a2cfd67ff17cda862db89380cea58d01f211a6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(name="grit-odr",
-                 author="Cillian Brewitt",
+                 author="Anonymous Author",
                  version='0.1.0',
                  install_requires=requirements,
                  packages=setuptools.find_packages(exclude=['analysis', 'test', 'scripts'])
