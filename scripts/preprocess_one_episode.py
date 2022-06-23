726e96f7088d3f3ced4681ba860898d95f3cec85
Massimiliano <s2105338@ed.ac.uk>
2022-06-23 09:41:38 +0100

    Fixed logic for lanes with no occlusions
    Former-commit-id: b0461a45663fac363ba31d568d639f2a7eb7dd8b

--- scripts/preprocess_one_episode.py
+++ scripts/preprocess_one_episode.py
@@ -1,7 +1,9 @@
 import argparse
+import os
 from datetime import datetime
 
 from grit.core.data_processing import prepare_episode_dataset
+from grit.core.base import get_data_dir
 
 
 def main():
@@ -10,6 +12,11 @@ def main():
     parser.add_argument('--episode_idx', type=int, help='Name of scenario to process', default=0)
 
     args = parser.parse_args()
+
+    # Skip files for which we already have data.
+    file_name = get_data_dir() + '{}_e{}.csv'.format(args.scenario, args.episode_idx)
+    if os.path.isfile(file_name):
+        return
 
     start = datetime.now()
     prepare_episode_dataset((args.scenario, args.episode_idx, True))
