919ba63bafe44b01147d96874e79a958051324bb
Cillian Brewitt <cillianbrewitt@gmail.com>
2022-06-27 17:20:40 +0100

    update author names
    Former-commit-id: 21cbabc0b20b6e8ccd4980d1f0e572af2d1a779f

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(name="grit-odr",
-                 author="Anonymous Author",
+                 author="Cillian Brewitt, Massimiliano Tamborski",
                  version='0.1.0',
                  install_requires=requirements,
                  packages=setuptools.find_packages(exclude=['analysis', 'test', 'scripts'])
