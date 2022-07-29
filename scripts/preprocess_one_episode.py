76b5c7aaa8c82b66510abd8998b86454d32c7208
Massimiliano <s2105338@ed.ac.uk>
2022-07-29 13:01:56 +0100

    Changed arguments to preprocess one episode

--- scripts/preprocess_one_episode.py
+++ scripts/preprocess_one_episode.py
@@ -7,13 +7,13 @@ def main():
     parser = argparse.ArgumentParser(description='Process the dataset')
     parser.add_argument('--scenario', type=str, help='Name of scenario to process', default="bendplatz")
     parser.add_argument('--episode_idx', type=int, help='Name of scenario to process', default=0)
-    parser.add_argument('--extract_indicator_features', help='If you want to extract the indicator features',
-                        action='store_true')
+    parser.add_argument('--no_indicator_features', help='If you don`t want to extract the indicator features',
+                        action='store_false')
 
     args = parser.parse_args()
 
     start = datetime.now()
-    prepare_episode_dataset((args.scenario, args.episode_idx, args.extract_indicator_features))
+    prepare_episode_dataset((args.scenario, args.episode_idx, args.no_indicator_features))
     print(datetime.now() - start)
 
 
