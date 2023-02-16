93e3e91b655f9b9b141af733a710fc1966a88da8
Cheng, Wang <wang.ch@fzd.tu-darmstadt.de>
2023-02-16 14:54:27 +0800

    split sceanrio2episode

--- scripts/opendd_conversion.py
+++ scripts/opendd_conversion.py
@@ -30,8 +30,103 @@ def compute_frames(df):
 
     return initial_frames_dict, final_frames_dict
 
+# save episode to csv
+def episode2csv(recordingID: int, trajectories: pd.DataFrame, origin:list[float]):
+    recordingID = str(recordingID).zfill(2)
+    """ tracks file """
+    new_trajectories = pd.DataFrame(columns=["recordingId", "trackId", "frame", "trackLifetime", "xCenter", 'yCenter',
+                                             "heading", "width", "length", "xVelocity", 'yVelocity',
+                                             "xAcceleration", "yAcceleration", "lonAcceleration", "latAcceleration"])
 
-recordingID = 200  # TODO: change
+    # The track_id / agent_id must be zero based.
+    initial_agent_id = min(trajectories["OBJID"])
+    new_trajectories["recordingId"] = [recordingID for _ in range(len(trajectories["OBJID"]))]
+    new_trajectories["trackId"] = np.array(trajectories["OBJID"]) - initial_agent_id
+    new_trajectories["frame"] = round(trajectories["TIMESTAMP"] / 0.033366)
+
+    new_trajectories["trackLifetime"] = compute_track_lifetime(new_trajectories)
+    new_trajectories["xCenter"] = np.array(trajectories["UTM_X"]) - origin[0]
+    new_trajectories["yCenter"] = np.array(trajectories["UTM_Y"]) - origin[1]
+
+    new_trajectories["heading"] = np.rad2deg(trajectories["UTM_ANGLE"])
+    new_trajectories["width"] = trajectories["WIDTH"]
+    new_trajectories["length"] = trajectories["LENGTH"]
+
+    new_trajectories["xVelocity"] = np.array(trajectories["V"]) * np.cos(trajectories["UTM_ANGLE"])
+    new_trajectories["yVelocity"] = np.array(trajectories["V"]) * np.sin(trajectories["UTM_ANGLE"])
+    new_trajectories["xAcceleration"] = np.array(trajectories["ACC"]) * np.cos(
+        trajectories["UTM_ANGLE"])
+    new_trajectories["yAcceleration"] = np.array(trajectories["ACC"]) * np.sin(
+        trajectories["UTM_ANGLE"])
+
+    new_trajectories.to_csv(data_path + "/" +recordingID + "_tracks.csv", index=False)
+
+    """ Recording meta file """
+    # From the World File, the last two numbers represent the UTM-x and -y coordinates of the top left corner
+    recording_meta = pd.DataFrame(columns=["recordingId", "locationId", "frameRate", "speedLimit", "weekday",
+                                           "startTime", "duration", "numTracks", "numVehicles", "numVRUs",
+                                           "latLocation", "lonLocation", "xUtmOrigin", "yUtmOrigin", "orthoPxToMeter"])
+
+    recording_meta["recordingId"] = [recordingID]
+    recording_meta["locationId"] = np.nan
+    recording_meta["frameRate"] = [30]
+    recording_meta["weekday"] = np.nan
+    recording_meta["startTime"] = [np.min(trajectories["TIMESTAMP"])]
+    recording_meta["duration"] = [
+        np.max(trajectories["TIMESTAMP"]) - np.min(trajectories["TIMESTAMP"])]
+    recording_meta["numTracks"] = np.nan
+    recording_meta["numVehicles"] = [len(new_trajectories["recordingId"].unique())]
+    recording_meta["numVRUs"] = np.nan
+    recording_meta["latLocation"] = np.nan
+    recording_meta["lonLocation"] = np.nan
+    recording_meta["xUtmOrigin"] = origin[0]
+    recording_meta["yUtmOrigin"] = origin[1]
+    recording_meta["orthoPxToMeter"] = np.nan
+
+    recording_meta.to_csv(data_path + recordingID + "_recordingMeta.csv", index=False)
+
+    """ Tracks meta file """
+    tracks_meta = pd.DataFrame(columns=["recordingId", "trackId", "initialFrame", "finalFrame", "numFrames",
+                                        "width", "length", "class"])
+
+    initial_frames_dict, final_frames_dict = compute_frames(new_trajectories)
+
+    tracks_meta["recordingId"] = [recordingID for _ in range(len(trajectories["OBJID"]))]
+    tracks_meta["trackId"] = new_trajectories["trackId"]
+    tracks_meta["initialFrame"] = [initial_frames_dict[aid] for aid in new_trajectories["trackId"]]
+    tracks_meta["finalFrame"] = [final_frames_dict[aid] for aid in new_trajectories["trackId"]]
+
+    # the extra frame is because the frame id is zero-based
+    tracks_meta["numFrames"] = tracks_meta["finalFrame"] - tracks_meta["initialFrame"] + 1
+
+    tracks_meta["width"] = trajectories["WIDTH"]
+    tracks_meta["length"] = trajectories["LENGTH"]
+
+    class_equivalent = {"Car": "car", "Bus": "bus", "Medium Vehicle": "truck_bus", "Heavy Vehicle": "truck_bus"}
+    tracks_meta["class"] = [class_equivalent[vehicle] for vehicle in trajectories["CLASS"]]
+
+    tracks_meta.drop_duplicates(subset=["trackId"], inplace=True)
+    tracks_meta.to_csv(data_path + recordingID + "_tracksMeta.csv", index=False)
+
+# split one scenario into episode
+def datatframe2episode(original_trajectories: pd.DataFrame, origin:list[float]):
+    # each 1000 vehicles are stored in one episode
+    vehicles_each_episode = 1000
+    IDs = original_trajectories["OBJID"]
+    initial_agent_id = min(IDs)
+    vehicle_num = max(IDs) - initial_agent_id + 1
+    recordingID = 0
+    start_inx = 0
+    for inx, id in enumerate(IDs):
+        if vehicle_num >= vehicles_each_episode:
+            if id - initial_agent_id == 1000:
+                episode2csv(recordingID, original_trajectories[start_inx:inx], origin)
+                start_inx = inx
+                vehicle_num = vehicle_num - vehicles_each_episode
+                recordingID +=1
+        else:
+            episode2csv(recordingID, original_trajectories[initial_agent_id:id], origin)
+            break
 
 
 if __name__ == "__main__":
@@ -41,79 +136,9 @@ if __name__ == "__main__":
     original_trajectories = pd.read_csv(data_path + scenario_name + "_trajectories.csv")
     scenario_config = ScenarioConfig.load(scenario_dir + f"/configs/{scenario_name}.json")
 
-    x_origin = scenario_config.map_center_utm[0]
-    y_origin = scenario_config.map_center_utm[1]
+    origin = [scenario_config.map_center_utm[0], scenario_config.map_center_utm[1]]
 
-    """ tracks file """
-    new_trajectories = pd.DataFrame(columns=["recordingId", "trackId", "frame", "trackLifetime", "xCenter", 'yCenter',
-                                             "heading", "width", "length", "xVelocity", 'yVelocity',
-                                             "xAcceleration", "yAcceleration", "lonAcceleration", "latAcceleration"])
+    datatframe2episode(original_trajectories, origin)
 
-    # The track_id / agent_id must be zero based.
-    initial_agent_id = min(original_trajectories["OBJID"])
-    new_trajectories["recordingId"] = [recordingID for _ in range(len(original_trajectories["OBJID"]))]
-    new_trajectories["trackId"] = np.array(original_trajectories["OBJID"]) - initial_agent_id
-    new_trajectories["frame"] = round(original_trajectories["TIMESTAMP"] / 0.033366)
-
-    new_trajectories["trackLifetime"] = compute_track_lifetime(new_trajectories)
-    new_trajectories["xCenter"] = np.array(original_trajectories["UTM_X"]) - x_origin
-    new_trajectories["yCenter"] = np.array(original_trajectories["UTM_Y"]) - y_origin
-
-    new_trajectories["heading"] = np.rad2deg(original_trajectories["UTM_ANGLE"])
-    new_trajectories["width"] = original_trajectories["WIDTH"]
-    new_trajectories["length"] = original_trajectories["LENGTH"]
-
-    new_trajectories["xVelocity"] = np.array(original_trajectories["V"]) * np.cos(original_trajectories["UTM_ANGLE"])
-    new_trajectories["yVelocity"] = np.array(original_trajectories["V"]) * np.sin(original_trajectories["UTM_ANGLE"])
-    new_trajectories["xAcceleration"]= np.array(original_trajectories["ACC"])*np.cos(original_trajectories["UTM_ANGLE"])
-    new_trajectories["yAcceleration"]= np.array(original_trajectories["ACC"])*np.sin(original_trajectories["UTM_ANGLE"])
-
-    new_trajectories.to_csv(data_path + "/200_tracks.csv", index=False)
-
-    """ Recording meta file """
-    # From the World File, the last two numbers represent the UTM-x and -y coordinates of the top left corner
-    recording_meta = pd.DataFrame(columns=["recordingId", "locationId", "frameRate", "speedLimit", "weekday",
-                                           "startTime", "duration", "numTracks", "numVehicles", "numVRUs",
-                                           "latLocation", "lonLocation", "xUtmOrigin", "yUtmOrigin", "orthoPxToMeter"])
-
-    recording_meta["recordingId"] = [200]
-    recording_meta["locationId"] = np.nan
-    recording_meta["frameRate"] = [30]
-    recording_meta["weekday"] = np.nan
-    recording_meta["startTime"] = [np.min(original_trajectories["TIMESTAMP"])]
-    recording_meta["duration"] = [np.max(original_trajectories["TIMESTAMP"]) - np.min(original_trajectories["TIMESTAMP"])]
-    recording_meta["numTracks"] = np.nan
-    recording_meta["numVehicles"] = [len(new_trajectories["recordingId"].unique())]
-    recording_meta["numVRUs"] = np.nan
-    recording_meta["latLocation"] = np.nan
-    recording_meta["lonLocation"] = np.nan
-    recording_meta["xUtmOrigin"] = [x_origin]
-    recording_meta["yUtmOrigin"] = [y_origin]
-    recording_meta["orthoPxToMeter"] = np.nan
-
-    recording_meta.to_csv(data_path + "200_recordingMeta.csv", index=False)
-
-    """ Tracks meta file """
-    tracks_meta = pd.DataFrame(columns=["recordingId", "trackId", "initialFrame", "finalFrame", "numFrames",
-                                        "width", "length", "class"])
-
-    initial_frames_dict, final_frames_dict = compute_frames(new_trajectories)
-
-    tracks_meta["recordingId"] = [recordingID for _ in range(len(original_trajectories["OBJID"]))]
-    tracks_meta["trackId"] = new_trajectories["trackId"]
-    tracks_meta["initialFrame"] = [initial_frames_dict[aid] for aid in new_trajectories["trackId"]]
-    tracks_meta["finalFrame"] = [final_frames_dict[aid] for aid in new_trajectories["trackId"]]
-
-    # the extra frame is because the frame id is zero-based
-    tracks_meta["numFrames"] = tracks_meta["finalFrame"] - tracks_meta["initialFrame"] + 1
-
-    tracks_meta["width"] = original_trajectories["WIDTH"]
-    tracks_meta["length"] = original_trajectories["LENGTH"]
-
-    class_equivalent = {"Car": "car", "Bicycle": "bicycle", "Medium Vehicle": "truck_bus", "Heavy Vehicle": "truck_bus"}
-    tracks_meta["class"] = [class_equivalent[vehicle] for vehicle in original_trajectories["CLASS"]]
-
-    tracks_meta.drop_duplicates(subset=["trackId"], inplace=True)
-    tracks_meta.to_csv(data_path + "200_tracksMeta.csv", index=False)
 
 
