88cd558d2f23938a9f6ac1f2c752c756f120fe20
Cillian Brewitt <cillianbrewitt@gmail.com>
2023-02-25 23:35:48 +0000

    update goal generator to work with rdb2

--- analysis/playground.py
+++ analysis/playground.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 from igp2 import Map, plot_map
 from igp2.data.scenario import ScenarioConfig, InDScenario
 
-scenario_name = 'rdb7'
+scenario_name = 'rdb2'
 episode_idx = 0
 config = ScenarioConfig.load(f"scenarios/configs/{scenario_name}.json")
 scenario_map = Map.parse_from_opendrive(f"scenarios/maps/{scenario_name}.xodr")
 scenario = InDScenario(config)
-episode = scenario.load_episode(episode_idx)
+#episode = scenario.load_episode(episode_idx)
 
 plot_map(scenario_map, scenario_config=config, plot_background=False)
 
-for agent in episode.agents.values():
-    if agent.metadata.agent_type == 'car':
-        path = agent.trajectory.path
-        plt.plot(path[:, 0], path[:, 1], linewidth=0.5)
-        print(agent.agent_id)
-        break
+# for agent in episode.agents.values():
+#     if agent.metadata.agent_type == 'car':
+#         path = agent.trajectory.path
+#         plt.plot(path[:, 0], path[:, 1], linewidth=0.5)
+#         print(agent.agent_id)
+#         break
 
 # lanes = scenario_map.lanes_at((24.0, -46.9), max_distance=0.01)
 # print(lanes)
